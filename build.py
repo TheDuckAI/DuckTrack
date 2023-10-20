e9346ade548a2abb8f396e0e880ec13c93817107
Broyojo <dha@xoba.com>
2023-10-20 18:15:31 -0400

    improve build scrip

--- build.py
+++ build.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("pyinstaller --onefile --windowed --add-data \"assets/:assets/\" --name DuckTrack --icon=assets/hal9000.png main.py")
+os.system("rm -rf dist build && pyinstaller --onefile --windowed --add-data \"assets/:assets/\" --name DuckTrack --icon=assets/hal9000.png main.py")
