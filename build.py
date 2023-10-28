0d364720b820655d4674ebd6ac13288184e30ad6
Broyojo <dha@xoba.com>
2023-10-28 00:32:49 -0400

    improved build script and fixed icon on windows

--- build.py
+++ build.py
@@ -1,3 +1,27 @@
-import os
+import shutil
+import sys
+from pathlib import Path
+from platform import system
+from subprocess import CalledProcessError, run
 
-os.system("rm -rf dist build && pyinstaller --onefile --windowed --add-data \"assets/:assets/\" --name DuckTrack --icon=assets/hal9000.png main.py")
+project_dir = Path(".")
+assets_dir = project_dir / "assets"
+main_py = project_dir / "main.py"
+icon_file = assets_dir / ("hal9000.ico" if system() == "Windows" else "hal9000.png")
+
+for dir_to_remove in ["dist", "build"]:
+    dir_path = project_dir / dir_to_remove
+    if dir_path.exists():
+        shutil.rmtree(dir_path)
+
+pyinstaller_cmd = [
+    "pyinstaller", "--onefile", "--windowed",
+    f"--add-data={assets_dir}{';' if system() == 'Windows' else ':'}{assets_dir}",
+    f"--name=DuckTrack", f"--icon={icon_file}", str(main_py)
+]
+
+try:
+    run(pyinstaller_cmd, check=True)
+except CalledProcessError as e:
+    print("An error occurred while running PyInstaller:", e)
+    sys.exit(1)
